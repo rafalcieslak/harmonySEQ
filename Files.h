c812913377ee43f88caefb7b2cbf9d36a89e2185
Rafał Cieślak <rafalcieslak256@gmail.com>
2010-09-06 17:10:31 +0200

    Files* files added

---
+++ Files.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Files.h
+ * Author: cielak
+ *
+ * Created on 6 wrzesień 2010, 15:53
+ */
+
+#ifndef FILES_H
+#define	FILES_H
+
+#include "global.h"
+
+namespace Files{
+void SaveToFile();
+void LoadFromFile();
+}
+
+
+#endif	/* FILES_H */
+
