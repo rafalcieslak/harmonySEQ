a7a201453f6b14f039c5922069f78a1ccbe0f4e9
Rafał Cieślak <rafalcieslak256@gmail.com>
2010-09-06 20:05:05 +0200

    licence notices added to all files

--- debug.cpp
+++ debug.cpp
@@ -1,9 +1,21 @@
-/* 
- * File:   debug.cpp
- * Author: cielak
- * 
- * Created on 19 sierpień 2010, 14:42
- */
+/*
+    Copyright (C) 2010 Rafał Cieślak
+
+    This file is part of harmonySEQ.
+
+    HarmonySEQ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HarmonySEQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HarmonySEQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "debug.h"
 
