67db0438c244dd855f1bddc24d931aba554f0f28
Rafał Cieślak <rafalcieslak256@gmail.com>
2011-07-12 21:32:37 +0200

    added PatternWidget

---
+++ src/PatternWidget.h
@@ -0,0 +1,33 @@
+/*
+    Copyright (C)  2011 Rafał Cieślak
+
+    This file is part of harmonySEQ.
+
+    HarmonySEQ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HarmonySEQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HarmonySEQ.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PATTERNWIDGET_H
+#define	PATTERNWIDGET_H
+#include "gtkmm.h"
+/**PatternWidget is basically a GUI for NoteContainer, that uses DrawingArea to display a piano-roll interface.*/
+class PatternWidget : Gtk::DrawingArea {
+public:
+    PatternWidget();
+    virtual ~PatternWidget();
+private:
+
+};
+
+#endif	/* PATTERNWIDGET_H */
+
