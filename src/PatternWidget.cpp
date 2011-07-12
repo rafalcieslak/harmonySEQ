6974c600eaf6a100b1593252bb3495283958767a
Rafał Cieślak <rafalcieslak256@gmail.com>
2011-07-12 23:00:02 +0200

    removed unneeded header

--- src/PatternWidget.cpp
+++ src/PatternWidget.cpp
@@ -16,9 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with HarmonySEQ.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#include <gtk-2.0/gdk/gdkcairo.h>
-
 #include "PatternWidget.h"
 #include "cairomm/context.h"
 #include "global.h"
